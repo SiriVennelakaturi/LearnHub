15f1be5fe373b598fdb3bd4a85d8a1383b943098
SiriVennelakaturi <159786922+SiriVennelakaturi@users.noreply.github.com>
2025-07-19 09:56:04 +0530

    Delete middleware directory

--- middleware/isAdmin.js
+++
@@ -1,8 +0,0 @@
-// middleware/isAdmin.js
-export const isAdmin = (req, res, next) => {
-  if (req.user?.type === 'admin') {
-    next();
-  } else {
-    res.status(403).json({ message: 'Access denied. Admins only.' });
-  }
-};
